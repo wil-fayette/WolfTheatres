d9729a4418759d33136b27e1cd160cf72a6ae4ee
wil-fayette <wil.fayette@gmail.com>
2014-06-01 22:06:25 -0400

    showtimes page progress

--- WolfTheatres/App/Scripts/app.js
+++ WolfTheatres/App/Scripts/app.js
@@ -1,4 +1,4 @@
-﻿var WolfTheatresApp = angular.module("WolfTheatresApp", ["ngResource", "ngRoute"]).config(function ($routeProvider) {
+﻿var WolfTheatresApp = angular.module("WolfTheatresApp", ["ngResource", "ngRoute", "ui.bootstrap"]).config(function ($routeProvider) {
     $routeProvider.
         when('/', { controller: 'HomeController', templateUrl: 'App/Views/home.html' }).
         when('/employees', { controller: 'EmployeesController', templateUrl: 'App/Views/employees.html' }).
@@ -11,16 +11,79 @@ WolfTheatresApp.controller('NavigationController', function ($scope, $location) 
         var currentRoute = $location.path().substring(1) || 'home';
         return page === currentRoute ? 'active' : '';
     }
-});
-
-WolfTheatresApp.controller('HomeController', function ($scope) {
+}).controller('HomeController', function ($scope) {
     $scope.message = 'cat';
-});
-
-WolfTheatresApp.controller('EmployeesController', function ($scope) {
+}).controller('EmployeesController', function ($scope) {
     $scope.message = 'dog';
+}).controller('MoviesController', function ($scope, $http){ 
+    var apiKey = 'pup7g2wafuxya22ryzfvyung';
+    $scope.wolfMovies = [];
+    $scope.rottenMovies = [];
+    
+    function getBoxOfficeMovies(callback) {
+        $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json', {
+            params: {
+                apikey: apiKey,
+                limit: 50,
+                callback: 'JSON_CALLBACK'
+            }
+        }).success(function (data) {
+            callback(data);
+        });
+    }
+
+    function getUpcomingMovies(callback) {
+        $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/upcoming.json', {
+            params: {
+                apikey: apiKey,
+                page_limit: 50,
+                callback: 'JSON_CALLBACK'
+            }
+        }).success(function (data) {
+            callback(data);
+        });
+    }
+
+    function addMovie(movie) {
+        var inArray = false;
+        for (var i = 0; i < $scope.wolfMovies.length; i++) {
+            if ($scope.wolfMovies[i] == movie) {
+                inArray = true;
+            }
+        }
+
+        if (inArray != true) {
+            $scope.wolfMovies.push(movie);
+        }
+    }
+
+    $scope.addMovie = addMovie;
+    
+    function refreshMovies() {
+        getBoxOfficeMovies(function (boxOfficeMovies) {
+            getUpcomingMovies(function (upcomingMovies) {
+                var rottenMovies = upcomingMovies.movies.concat(boxOfficeMovies.movies)
+                for (var i = 0; i < rottenMovies.length; i++) {
+                    if (rottenMovies[i].posters.detailed == "http://images.rottentomatoescdn.com/images/redesign/poster_default.gif") {
+                        rottenMovies.splice(i, 1);
+                    }
+                }
+                $scope.rottenMovies = rottenMovies;
+            });
+        });
+    }
+
+    refreshMovies();
+
+    
+
+
+
+    //TODO
+    //call out to webapi and populate wolfMovies with movies in our database
+
+
+}).controller('ShowtimesController', function ($scope) {
+
 });
 
-WolfTheatresApp.controller('ShowtimesController', function ($scope) {
-    $scope.message = 'racoon';
-});
